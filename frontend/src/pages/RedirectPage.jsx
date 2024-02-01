1128fdf031412dffccc35c27e2360ccfc04356be
Loi Vo Tan <dev.loivo2k2@gmail.com>
2024-02-02 03:33:31 +0700

    style(redirect page): design redirect page

---
+++ frontend/src/pages/RedirectPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const RedirectPage = (props) => {
+    // const auth = useAuth();
+	// const navigate = useNavigate();
+	// React.useEffect(() => {
+	// 	if (auth.user?.roleName === Role.ADMIN) {
+	// 		navigate('/home');
+	// 	}
+	// 	if (auth.user?.roleName === Role.EMPLOYEE) {
+	// 		navigate('/table');
+	// 	}
+	// 	if (auth.user?.roleName === Role.CHEF) {
+	// 		navigate('/menu');
+	// 	}
+	// }, [auth.user?.roleName]);
+	return <></>;
+};
+
+export default RedirectPage;
